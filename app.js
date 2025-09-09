a6e3da360aeca0eb3232bf8f6c683a5fe802f410
Dale Simkin <dale.simkin32@gmail.com>
2025-09-09 12:04:39 +0100

    part way through

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Halo Whirled!");
+
+const blogPosts = [
+  "My first blog post",
+  "My second blog post",
+  "My third blog post",
+];
+
+console.log(blogPosts);
+
+console.log(blogPosts[0]);
+console.log(blogPosts[1]);
+console.log(blogPosts[2]);
+
+blogPosts[3] = "My fourth blog post";
+
+console.log(blogPosts[3]);
