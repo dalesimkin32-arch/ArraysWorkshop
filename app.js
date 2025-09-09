6a51660008dc57579d3d910b93718ce035a6ff64
Dale Simkin <dale.simkin32@gmail.com>
2025-09-09 13:26:26 +0100

    arrays done for now

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ console.log(blogPosts[2]);
 blogPosts[3] = "My fourth blog post";
 
 console.log(blogPosts[3]);
+
+const favFoods = [
+  "lamb madras",
+  "marlin steak",
+  "beef wellington",
+  "crispy duck pancakes",
+];
+
+console.log(favFoods);
+
+const favColours = ["crimson", "cerulean", "azure", "malachite", "magenta"];
+
+console.log(favColours);
+
+const favNumbers = [3.14159, 299792458, 8.98755179e16, 6.626e-34, 2.71828];
+
+console.log(favNumbers);
